21e44d72626baae8fc06fa4b16d2280945f31450
Máté Homolya <mate.homolya@gmail.com>
2023-03-03 22:23:01 -0800

    Touch pan decelerate

--- src/scroll.ts
+++ src/scroll.ts
@@ -16,6 +16,11 @@ export function useScroll({
   const draggingRef = useRef(dragging)
   const mouseDownRef = useRef(mouseDown)
   const originRef = useRef({ x: 0, y: 0 })
+  const touchPanRef = useRef(false)
+  const touchZoomRef = useRef(false)
+  const touchDecelerateRef = useRef(false)
+  const prevPointerRef = useRef({ x: 0, y: 0 })
+  const velocityRef = useRef({ x: 0, y: 0 })
   useEffect(() => {
     const el = node.current
     if (!el) return
@@ -78,7 +83,6 @@ export function useScroll({
         x: px,
         y: py,
       }
-      // console.log('origin ', originRef.current)
 
       if (draggingRef.current && mouseDownRef.current) {
         ev.preventDefault()
@@ -97,6 +101,11 @@ export function useScroll({
       if (e.button === 0 && !mouseDownRef.current) {
         setMouseDown(true)
         mouseDownRef.current = true
+      } else if (e.button === 1 && !mouseDownRef.current) {
+        mouseDownRef.current = true
+        draggingRef.current = true
+        setDragging(true)
+        setMouseDown(true)
       }
     }
     const pointerUp = (ev: Event) => {
@@ -104,6 +113,11 @@ export function useScroll({
       if (e.button === 0 && mouseDownRef.current) {
         setMouseDown(false)
         mouseDownRef.current = false
+      } else if (e.button === 1 && mouseDownRef.current) {
+        mouseDownRef.current = false
+        draggingRef.current = false
+        setDragging(false)
+        setMouseDown(false)
       }
     }
     const handleKeyDown = (ev: Event) => {
@@ -123,12 +137,129 @@ export function useScroll({
       }
     }
 
+    const touchStart = (ev: Event) => {
+      const e = ev as TouchEvent
+      if (e.touches.length === 2) {
+        // Zoom gesture
+        touchZoomRef.current = true
+        const t1 = e.touches[0]
+        const t2 = e.touches[1]
+        const rect = el.getBoundingClientRect()
+        const p1 = {
+          x: t1.clientX - rect.left,
+          y: t1.clientY - rect.top,
+        }
+        const p2 = {
+          x: t2.clientX - rect.left,
+          y: t2.clientY - rect.top,
+        }
+        const center = {
+          x: (p1.x + p2.x) / 2,
+          y: (p1.y + p2.y) / 2,
+        }
+        originRef.current = center
+      } else {
+        // Pan gesture
+        touchPanRef.current = true
+        const rect = el.getBoundingClientRect()
+        originRef.current = {
+          x: e.touches[0].clientX - rect.left - transformRef.current.x,
+          y: e.touches[0].clientY - rect.top - transformRef.current.y,
+        }
+      }
+    }
+    const touchMove = (ev: Event) => {
+      const e = ev as TouchEvent
+      if (touchPanRef.current) {
+        // Pan gesture
+        ev.preventDefault()
+        const t1 = e.touches[0]
+        const rect = el.getBoundingClientRect()
+        const o = originRef.current
+        const delta = {
+          x: t1.clientX - rect.left,
+          y: t1.clientY - rect.top,
+        }
+        const p = prevPointerRef.current
+        const velocity = {
+          x: delta.x - p.x,
+          y: delta.y - p.y,
+        }
+        velocityRef.current = velocity
+        prevPointerRef.current = delta
+        setTransform((prev) => {
+          const nx = o.x - delta.x
+          const ny = o.y - delta.y
+          const newTranform = { x: -nx, y: -ny, scale: prev.scale }
+          transformRef.current = newTranform
+          return newTranform
+        })
+      }
+      if (e.touches.length === 2) {
+        ev.preventDefault()
+        const t1 = e.touches[0]
+        const t2 = e.touches[1]
+        const rect = el.getBoundingClientRect()
+        const p1 = {
+          x: t1.clientX - rect.left,
+          y: t1.clientY - rect.top,
+        }
+        const p2 = {
+          x: t2.clientX - rect.left,
+          y: t2.clientY - rect.top,
+        }
+        const center = {
+          x: (p1.x + p2.x) / 2,
+          y: (p1.y + p2.y) / 2,
+        }
+        const dx = center.x - originRef.current.x
+        const dy = center.y - originRef.current.y
+        setTransform((prev) => {
+          const nx = prev.x + dx
+          const ny = prev.y + dy
+          const newTranform = { x: nx, y: ny, scale: prev.scale }
+          transformRef.current = newTranform
+          return newTranform
+        })
+      }
+    }
+    const touchEnd = (ev: Event) => {
+      const e = ev as TouchEvent
+      if (touchPanRef) {
+        touchDecelerateRef.current = true
+      }
+    }
+
+    let cancelAnimation: number
+    const animate = () => {
+      const v = velocityRef.current
+      const dx = v.x
+      const dy = v.y
+      const damping = 0.9
+      velocityRef.current = { x: dx * damping, y: dy * damping }
+      const nx = transformRef.current.x + dx
+      const ny = transformRef.current.y + dy
+      setTransform((prev) => {
+        const newTranform = { x: nx, y: ny, scale: prev.scale }
+        transformRef.current = newTranform
+        return newTranform
+      })
+      cancelAnimation = requestAnimationFrame(animate)
+    }
+    animate()
+
     el.addEventListener('mousewheel', handleMouseWheel)
     window.addEventListener('keydown', handleKeyDown)
     window.addEventListener('keyup', handleKeyUp)
-    window.addEventListener('pointermove', pointerMove)
-    el.addEventListener('pointerdown', pointerDown)
-    el.addEventListener('pointerup', pointerUp)
+    const isTouch = 'ontouchstart' in window
+    if (isTouch) {
+      el.addEventListener('touchstart', touchStart)
+      el.addEventListener('touchmove', touchMove)
+    } else {
+      window.addEventListener('pointermove', pointerMove)
+      el.addEventListener('pointerdown', pointerDown)
+      el.addEventListener('pointerup', pointerUp)
+    }
 
     // on any event, update the cursor
     el.addEventListener('pointermove', updateCursor)
@@ -141,9 +272,17 @@ export function useScroll({
       el.removeEventListener('mousewheel', handleMouseWheel)
       window.removeEventListener('keydown', handleKeyDown)
       window.removeEventListener('keyup', handleKeyUp)
-      window.removeEventListener('pointermove', pointerMove)
-      el.removeEventListener('pointerdown', pointerDown)
-      el.removeEventListener('pointerup', pointerUp)
+
+      if (isTouch) {
+        el.removeEventListener('touchstart', touchStart)
+        el.removeEventListener('touchmove', touchMove)
+      } else {
+        window.removeEventListener('pointermove', pointerMove)
+        el.removeEventListener('pointerdown', pointerDown)
+        el.removeEventListener('pointerup', pointerUp)
+      }
+
+      cancelAnimationFrame(cancelAnimation)
 
       el.removeEventListener('pointermove', updateCursor)
       el.removeEventListener('pointerdown', updateCursor)
